610f38e42c354dcb00ba9dde2205214f89f60081
Hansjoerg Keser <hansjoerg.kese@idealo.de>
2019-05-22 09:54:51 +0200

    reformat code, optimize imports

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -4,82 +4,80 @@ import java.util.Comparator;
 /**
  * Created by hansjoergkeser on 19.07.17.
  */
-public class RomanNumerals {
+class RomanNumerals {
 
-    /**
-     * M = 1000
-     * D = 500
-     * C = 100
-     * L = 50
-     * X = 10
-     * V = 5
-     * I = 1
-    **/
+	String addTwoNumerals(String a, String b) {
 
-    private final String ORDER= "MDCLXVI";
+		//remove lesser numerals in front of bigger ones
+		String resultA = removeSubtractionNumerals(a);
+		String resultB = removeSubtractionNumerals(b);
 
-    public String addTwoNumerals(String a, String b) {
+		String result = resultA + resultB;
 
-        //remove lesser numerals in front of bigger ones
-        String resultA = removeSubtractionNumerals(a);
-        String resultB = removeSubtractionNumerals(b);
+		//now sort the sum of numerals due to roman order
+		String[] array = result.split("");
+		/*
+		 * M = 1000
+		 * D = 500
+		 * C = 100
+		 * L = 50
+		 * X = 10
+		 * V = 5
+		 * I = 1
+		 */
+		String ORDER = "MDCLXVI";
+		Arrays.sort(array, Comparator.comparingInt(ORDER::indexOf));
+		result = String.join("", array);
 
-        String result = resultA + resultB;
+		result = sumUpFiveDigitGroupsOfOneKind(result);
 
-        //now sort the sum of numerals due to roman order
-        String[] array = result.split("");
-        Arrays.sort(array, Comparator.comparingInt(ORDER::indexOf));
-        result = String.join("",array);
+		//finally all remaining four-digit numerals of a kind have to be summarized
+		if (result.contains("IIII")) {
+			result = result.replace("IIII", "IV");
+		}
+		if (result.contains("XXXX")) {
+			result = result.replace("XXXX", "XL");
+		}
+		if (result.contains("CCCC")) {
+			result = result.replace("CCCC", "CD");
+		}
+		return result;
 
-        result = sumUpFiveDigitGroupsOfOneKind(result);
+	}
 
-        //finally all remaining four-digit numerals of a kind have to be summarized
-        if (result.contains("IIII")) {
-            result = result.replace("IIII", "IV");
-        }
-        if (result.contains("XXXX")) {
-            result = result.replace("XXXX", "XL");
-        }
-        if (result.contains("CCCC")) {
-            result = result.replace("CCCC", "CD");
-        }
-        return result;
+	private String removeSubtractionNumerals(String result) {
 
-    }
+		result = result.replaceAll("IV", "IIII");
+		result = result.replaceAll("IX", "VIIII");
+		result = result.replaceAll("XL", "XXXX");
+		result = result.replaceAll("CD", "CCCC");
+		result = result.replaceAll("CM", "DCCCC");
+		return result;
 
-    private String removeSubtractionNumerals(String result) {
+	}
 
-        result = result.replaceAll("IV","IIII");
-        result = result.replaceAll("IX","VIIII");
-        result = result.replaceAll("XL","XXXX");
-        result = result.replaceAll("CD","CCCC");
-        result = result.replaceAll("CM","DCCCC");
-        return result;
+	private String sumUpFiveDigitGroupsOfOneKind(String result) {
 
-    }
+		if (result.contains("IIIII")) {
+			result = result.replace("IIIII", "V");
+		}
+		if (result.contains("VV")) {
+			result = result.replace("VV", "X");
+		}
+		if (result.contains("XXXXX")) {
+			result = result.replace("XXXXX", "L");
+		}
+		if (result.contains("LL")) {
+			result = result.replace("LL", "C");
+		}
+		if (result.contains("CCCCC")) {
+			result = result.replace("CCCCC", "D");
+		}
+		if (result.contains("DD")) {
+			result = result.replace("DD", "M");
+		}
+		return result;
 
-    private String sumUpFiveDigitGroupsOfOneKind(String result) {
-
-        if (result.contains("IIIII")) {
-            result = result.replace("IIIII", "V");
-        }
-        if (result.contains("VV")) {
-            result = result.replace("VV", "X");
-        }
-        if (result.contains("XXXXX")) {
-            result = result.replace("XXXXX", "L");
-        }
-        if (result.contains("LL")) {
-            result = result.replace("LL", "C");
-        }
-        if (result.contains("CCCCC")) {
-            result = result.replace("CCCCC", "D");
-        }
-        if (result.contains("DD")) {
-            result = result.replace("DD", "M");
-        }
-        return result;
-
-    }
+	}
 
 }
--- src/test/java/ConvertToDecimalsTest.java
+++ src/test/java/ConvertToDecimalsTest.java
@@ -6,58 +6,58 @@ import org.junit.Test;
  */
 public class ConvertToDecimalsTest {
 
-    ArabicDecimals decimals = new ArabicDecimals();
+	private ArabicDecimals decimals = new ArabicDecimals();
 
-    @Test
-    public void assertConvertToOneTest() {
-        String input = "I";
-        int expectedResult = 1;
-        Assert.assertEquals("assertConvertToOneTest failed.", expectedResult, decimals.convert(input));
-    }
+	@Test
+	public void assertConvertToOneTest() {
+		String input = "I";
+		int expectedResult = 1;
+		Assert.assertEquals("assertConvertToOneTest failed.", expectedResult, decimals.convert(input));
+	}
 
-    @Test
-    public void assertConvertToTwoTest() {
-        Assert.assertEquals("assertConvertToTwoTest failed.", 2, decimals.convert("II"));
-    }
+	@Test
+	public void assertConvertToTwoTest() {
+		Assert.assertEquals("assertConvertToTwoTest failed.", 2, decimals.convert("II"));
+	}
 
-    @Test
-    public void assertConvertToThreeTest() {
-        Assert.assertEquals("assertConvertToThreeTest failed.", 3, decimals.convert("III"));
-    }
+	@Test
+	public void assertConvertToThreeTest() {
+		Assert.assertEquals("assertConvertToThreeTest failed.", 3, decimals.convert("III"));
+	}
 
-    @Test
-    public void assertConvertToFourTest() {
-        Assert.assertEquals("assertConvertToFourTest failed.", 4, decimals.convert("IV"));
-    }
+	@Test
+	public void assertConvertToFourTest() {
+		Assert.assertEquals("assertConvertToFourTest failed.", 4, decimals.convert("IV"));
+	}
 
-    @Test
-    public void assertConvertToFiveTest() {
-        Assert.assertEquals("assertConvertToFiveTest failed.", 5, decimals.convert("V"));
-    }
+	@Test
+	public void assertConvertToFiveTest() {
+		Assert.assertEquals("assertConvertToFiveTest failed.", 5, decimals.convert("V"));
+	}
 
-    @Test
-    public void assertConvertToSixTest() {
-        Assert.assertEquals("assertConvertToSixTest failed.", 6, decimals.convert("VI"));
-    }
+	@Test
+	public void assertConvertToSixTest() {
+		Assert.assertEquals("assertConvertToSixTest failed.", 6, decimals.convert("VI"));
+	}
 
-    @Test
-    public void assertConvertToSevenTest() {
-        Assert.assertEquals("assertConvertToSevenTest failed.", 7, decimals.convert("VII"));
-    }
+	@Test
+	public void assertConvertToSevenTest() {
+		Assert.assertEquals("assertConvertToSevenTest failed.", 7, decimals.convert("VII"));
+	}
 
-    @Test
-    public void assertConvertToEightTest() {
-        Assert.assertEquals("assertConvertToEightTest failed.", 8, decimals.convert("VIII"));
-    }
+	@Test
+	public void assertConvertToEightTest() {
+		Assert.assertEquals("assertConvertToEightTest failed.", 8, decimals.convert("VIII"));
+	}
 
-    @Test
-    public void assertConvertToNineTest() {
-        Assert.assertEquals("assertConvertToNineTest failed.", 9, decimals.convert("IX"));
-    }
+	@Test
+	public void assertConvertToNineTest() {
+		Assert.assertEquals("assertConvertToNineTest failed.", 9, decimals.convert("IX"));
+	}
 
-    @Test
-    public void assertConvertToTenTest() {
-        Assert.assertEquals("assertConvertToTenTest failed.", 10, decimals.convert("X"));
-    }
+	@Test
+	public void assertConvertToTenTest() {
+		Assert.assertEquals("assertConvertToTenTest failed.", 10, decimals.convert("X"));
+	}
 
 }
